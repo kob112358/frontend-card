041fe0ce640b0cd2c86c2c1359f4ac74a27c8ea6
Eric Kobliska <kob112358@gmail.com>
2022-08-09 11:21:36 -0500

    added placeholders to the front card - all finished!

--- src/componenets/BackCard.js
+++ src/componenets/BackCard.js
@@ -3,9 +3,13 @@ import FrontCard from "./FrontCard";
 import "./BackCard.css";
 
 function BackCard(props) {
+  let cvc = '000';
+  if(props?.cvc?.length > 0) {
+    cvc = props.cvc;
+  }
   return (
     <div className="background-card">
-      <div className="background-card__cvc">{props.cvc}</div>
+      <div className="background-card__cvc">{cvc}</div>
       <FrontCard
         name={props.name}
         number={props.number}
--- src/componenets/FrontCard.js
+++ src/componenets/FrontCard.js
@@ -3,22 +3,31 @@ import "./FrontCard.css";
 import { ReactComponent as YourSvg } from "../images/card-logo.svg";
 
 const FrontCard = (props) => {
-
   function monthYearDisplay(expDate) {
-    return String(expDate).padStart(2, '0');
+    return String(expDate).padStart(2, "0");
   }
 
-  const cardNumber = `${props.number.slice(0,4)} ${props.number.slice(4,8)} ${props.number.slice(8,12)} ${props.number.slice(12,16)}`;
+  let cardNumber = "0000 0000 0000 0000";
+  if (props.number.length > 0) {
+      cardNumber = `${props.number.slice(0, 4)} ${props.number.slice(4, 8)} ${props.number.slice(8, 12)} ${props.number.slice(12, 16)}`;
+    };
+  let cardName = "JANE APPLESEED";
+  if(props?.name?.length > 0) {
+    cardName = props.name;
+  }
+
+  
 
   return (
     <div className="display-card">
       <YourSvg className="svg" />
-      <div className="display-card__number">{cardNumber ? cardNumber : '0000 0000 0000 0000'}</div>
+      <div className="display-card__number">
+        {cardNumber}
+      </div>
       <div className="display-card__info">
-        {props.name}
+        {cardName}
         <div>
-          {monthYearDisplay(props.month)}/
-          {monthYearDisplay(props.year)}
+          {monthYearDisplay(props.month)}/{monthYearDisplay(props.year)}
         </div>
       </div>
     </div>
